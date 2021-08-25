8fa4268d0458a525801efff1e9d918bef82aadd2
PriyankaP1208 <ppriyapatil1208@gmail.com>
2021-08-25 11:50:48 +0530

    feat:added connection with server

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
-console.log("Welcome to Address Book");
+const express = require('express');
+const mongoose = require('mongoose');
+
+// create express app
+const app = express();
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }))
+
+// parse requests of content-type - application/json
+app.use(express.json())
+
+// define a simple route
+app.get('/', (req, res) => {
+    res.json({"message": "Welcome to Address Book Application"});
+});
+
+// listen for requests
+app.listen(3000, () => {
+    console.log("Server is listening on port 3000");
+});
