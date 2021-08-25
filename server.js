c5f94e6f376b034f5b4ce363d9c0d612a698b65c
PriyankaP1208 <ppriyapatil1208@gmail.com>
2021-08-25 11:58:44 +0530

    feat:added connection with database

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const dbConfig = require('./config/config');
 
 // create express app
 const app = express();
@@ -9,6 +10,19 @@ app.use(express.urlencoded({ extended: true }))
 
 // parse requests of content-type - application/json
 app.use(express.json())
+
+mongoose.Promise = global.Promise;
+
+// Connecting to the database
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("Successfully connected to the database");    
+}).catch(err => {
+    console.log('Could not connect to the database. Exiting now...', err);
+    process.exit();
+});
 
 // define a simple route
 app.get('/', (req, res) => {
